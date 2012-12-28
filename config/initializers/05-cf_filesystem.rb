ce2dabb4f18af1067396e0bb7c9b154f702df536
Katsunori Kawaguchi <hamakata@gmail.com>
2012-12-28 10:13:24 +0900

    Fix CF filesystem autoconfig

--- config/initializers/05-cf_filesystem.rb
+++ config/initializers/05-cf_filesystem.rb
@@ -1,7 +1,10 @@
-vcap_services = JSON.parse(ENV["VCAP_SERVICES"])
-if (fs = vcap_services.values.flatten.find { |h| h["label"] =~ /filesystem/ })
-  fs_info = fs["credentials"]["internal"]
-  ATTACHMENTS_STORAGE_PATH = File.join(fs_info["local_path"], fs_info["name"])
-else
-  ATTACHMENTS_STORAGE_PATH = ""
+# filesystem autoconfig for Cloud Foundry
+if ENV["VCAP_SERVICES"]
+  vcap_services = JSON.parse(ENV["VCAP_SERVICES"])
+  if (fs = vcap_services.values.flatten.find { |h| h["label"] =~ /filesystem/ })
+    fs_info = fs["credentials"]["internal"]
+    attachments_storage_path = File.join(fs_info["local_path"], fs_info["name"])
+  end
 end
+
+ATTACHMENTS_STORAGE_PATH = attachments_storage_path || ""
